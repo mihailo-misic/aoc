9654c7af8a0232dab18381144b4b366d4d3ab3be
mihailo-misic <mihailo.misic@gmail.com>
2021-12-14 23:30:53 +0100

    ...

--- 2021/14/code.go
+++ 2021/14/code.go
@@ -2,17 +2,46 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	. "../utils"
 )
 
 func main() {
 	input := ReadFile("./sinput.txt")
-	ans := 0
 
-	for _, line := range input {
-		fmt.Println(line)
+	chain := ""
+	reactions := map[string]string{}
+
+	for i, line := range input {
+		if i == 0 {
+			chain = line
+			continue
+		}
+		if i == 1 {
+			continue
+		}
+
+		lineInfo := strings.Split(line, " -> ")
+		fmt.Println(lineInfo)
+		reactions[lineInfo[0]] = lineInfo[1]
 	}
 
-	fmt.Println("Answer", ans)
+	fmt.Println("reactions", reactions)
+
+	fmt.Println("chain", chain)
+	for step := 0; step < 10; step++ {
+		newChain := ""
+		for i := 0; i < len(chain)-1; i++ {
+			key := string(chain[i]) + string(chain[i+1])
+			newChain += string(chain[i]) + reactions[key]
+			if i == len(chain)-2 {
+				newChain += string(chain[i+1])
+			}
+		}
+		chain = newChain
+		fmt.Println("chain", chain)
+	}
+
+	fmt.Println("Answer", chain)
 }
