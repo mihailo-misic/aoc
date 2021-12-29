d76652c320feed8dc09dd70fde7173491a40db3c
mihailo-misic <mihailo.misic@gmail.com>
2021-12-29 15:40:43 +0100

    D18 P2 DONE

---
+++ 2021/19/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	. "../utils"
+)
+
+func main() {
+	lines := ReadFile("./input.txt")
+
+	for _, line := range lines {
+		// Code
+	}
+
+	fmt.Println("\nAnswer:")
+}
