0569fc1e14a30cbefafdd78afd1a649728272031
mihailo-misic <mihailo.misic@gmail.com>
2022-09-24 10:48:14 +0200

    Day21 - P2 WIP

--- 2021/21/p2/main.go
+++ 2021/21/p2/main.go
@@ -2,22 +2,20 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 	"strings"
+	"time"
 
 	. "github.com/mihailo-misic/aoc/util"
 )
 
 type Player struct {
-	Id         int
-	ThrowsLeft int
-	Position   int
-	Score      int
-}
-
-type Dice struct {
-	TimesRolled int
-	Number      int
+	Id       int
+	Position int
+	Score    int
+	Throws   int
+	History  []int
 }
 
 var answer int
@@ -25,73 +23,107 @@ var answer int
 var p1 = Player{Id: 1}
 var p2 = Player{Id: 2}
 
-const GOAL = 1000
+var p1Wins int64 = 0
+var p2Wins int64 = 0
+
+const GOAL = 11
+
+// 10 = 3.926645636s
+// 11 = 5.623761788s
+// 14 = 1h26m39.998895069s
 
 func main() {
+	start := time.Now()
 	lines := ReadFile("../input.txt")
 
-	d := Dice{0, 0}
 	for i, line := range lines {
 		s := strings.Split(line, ": ")
 
 		if i == 0 {
 			p1.Position, _ = strconv.Atoi(s[1])
+			p1.History = append(p1.History, p1.Position)
 		}
 		if i == 1 {
 			p2.Position, _ = strconv.Atoi(s[1])
+			p2.History = append(p2.History, p2.Position)
 		}
 	}
 
-	for {
-		p1.ThrowsLeft = 3
-		p2.ThrowsLeft = 3
+	// add 1-0 to open
+	// add 2-0 to open
+	// add 3-0 to open
+	// while open != empty
+	//   each open
+	//     add x+1-0 to open
+	//     add x+2-0 to open
+	//     add x+3-0 to open
+	//     remove that open (x-0)
 
-		for p1.ThrowsLeft > 0 {
-			p1.throwDice(&d)
-		}
-		if p1.Score >= GOAL {
-			break
-		}
+	// Game
+	//  p1 pos
+	//  p1 sco
+	//  p2 pos
+	//  p2 sco
+	//  turn
 
-		for p2.ThrowsLeft > 0 {
-			p2.throwDice(&d)
-		}
-		if p2.Score >= GOAL {
-			break
-		}
+	rollDice(p1, p2, 1, 0)
+	rollDice(p1, p2, 2, 0)
+	rollDice(p1, p2, 3, 0)
+
+	winner := p1
+	winnerWins := p1Wins
+	if p1Wins < p2Wins {
+		winner = p2
+		winnerWins = p2Wins
 	}
-
-	loser := p1
-	if p2.Score < p1.Score {
-		loser = p2
-	}
-	answer = loser.Score * d.TimesRolled
+	fmt.Printf("P%v wins with", winner.Id)
+	answer = int(winnerWins)
 
 	CopyToClipboard(strconv.Itoa(answer))
 	fmt.Println("\nAnswer:", answer)
+
+	fmt.Println("Time:", time.Since(start))
 }
 
-func (p *Player) throwDice(d *Dice) {
-	d.roll()
-	p.ThrowsLeft--
+func rollDice(p1, p2 Player, diceNum, throwCount int) {
+	throwerIdx := math.Mod(math.Floor(float64(throwCount)/3), 2)
 
-	p.Position += d.Number
-	p.Position = p.Position % 10
-	if p.Position == 0 {
-		p.Position = 10
+	if throwerIdx == 0 {
+		p1 = goTo(p1, diceNum)
+
+		if p1.Score >= GOAL {
+			p1Wins++
+			//fmt.Printf("[1] Wins: %v, P: %+v\n", p1Wins, p1)
+			return
+		}
+	}
+	if throwerIdx == 1 {
+		p2 = goTo(p2, diceNum)
+
+		if p2.Score >= GOAL {
+			p2Wins++
+			//fmt.Printf("[2] Wins: %v, P: %+v\n", p2Wins, p2)
+			return
+		}
 	}
 
-	if p.ThrowsLeft == 0 {
-		p.Score += p.Position
-		fmt.Println(p.Id, "is at", p.Score)
-	}
+	rollDice(p1, p2, 1, throwCount+1)
+	rollDice(p1, p2, 2, throwCount+1)
+	rollDice(p1, p2, 3, throwCount+1)
 }
 
-func (d *Dice) roll() {
-	d.TimesRolled++
-	d.Number++
-
-	if d.Number > 100 {
-		d.Number = 1
+func goTo(p Player, diceNum int) (mP Player) {
+	mP = Player{
+		Id:       p.Id,
+		Position: (p.Position+diceNum-1)%10 + 1,
+		Score:    p.Score,
+		Throws:   p.Throws + 1,
 	}
+	if mP.Throws == 3 {
+		mP.Score += mP.Position
+		mP.Throws = 0
+	}
+	mP.History = append(p.History, mP.Position)
+
+	return mP
 }
