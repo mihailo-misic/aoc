9a2abe678fb61534d08c1cd838aafcf71837111b
mihailo-misic <mihailo.misic@gmail.com>
2022-10-01 11:52:20 +0200

    Day22 - P1 DONE

--- 2021/22/main.go
+++ 2021/22/main.go
@@ -17,15 +17,16 @@ type Command struct {
 	Z      [2]int
 }
 
-func main() {
-	lines := ReadFile("./xsinput.txt")
+const PART = 2
 
-	input := make([]Command, len(lines))
-	reg := regexp.MustCompile(`^(\w+) x\=(\d+)..(\d+),y\=(\d+)..(\d+),z\=(\d+)..(\d+)$`)
+func main() {
+	lines := ReadFile("./input.txt")
+
+	commands := make([]Command, len(lines))
+	reg := regexp.MustCompile(`^(\w+) x\=(-?\d+)..(-?\d+),y\=(-?\d+)..(-?\d+),z\=(-?\d+)..(-?\d+)$`)
 
 	for idx, line := range lines {
 		res := reg.FindStringSubmatch(line)
-		fmt.Println("res", res)
 		XLower, _ := strconv.Atoi(res[2])
 		XUpper, _ := strconv.Atoi(res[3])
 		YLower, _ := strconv.Atoi(res[4])
@@ -33,17 +34,63 @@ func main() {
 		ZLower, _ := strconv.Atoi(res[6])
 		ZUpper, _ := strconv.Atoi(res[7])
 
-		input[idx] = Command{
+		commands[idx] = Command{
 			Action: res[1] == "on",
 			X:      [2]int{XLower, XUpper},
 			Y:      [2]int{YLower, YUpper},
 			Z:      [2]int{ZLower, ZUpper},
 		}
 	}
-	for _, r := range input {
+	for _, r := range commands {
 		fmt.Println(r)
+	}
+
+	if PART == 1 {
+		part1(commands)
+	} else {
+		part2(commands)
 	}
 
 	CopyToClipboard(strconv.Itoa(answer))
 	fmt.Println("\nAnswer:", answer)
+}
+
+func part2(commands []Command) {
+}
+
+func part1(commands []Command) {
+	cubeMap := map[[3]int]bool{}
+
+	for i, cmd := range commands {
+		fmt.Println("Doing", i, "/", len(commands))
+
+		if isOut(cmd.X) || isOut(cmd.Y) || isOut(cmd.Z) {
+			continue
+		}
+
+		for x := cmd.X[0]; x <= cmd.X[1]; x++ {
+			for y := cmd.Y[0]; y <= cmd.Y[1]; y++ {
+				for z := cmd.Z[0]; z <= cmd.Z[1]; z++ {
+					cubeMap[[3]int{x, y, z}] = cmd.Action
+				}
+			}
+		}
+	}
+
+	for _, on := range cubeMap {
+		if on {
+			answer++
+		}
+	}
+}
+
+const MIN = -50
+const MAX = 50
+
+func isOut(in [2]int) bool {
+	if (MIN <= in[0] && in[0] <= MAX) || (MIN <= in[1] && in[1] <= MAX) {
+		return false
+	}
+
+	return true
 }
